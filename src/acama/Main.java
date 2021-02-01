1183b0ced5ebf86ac77e2dc325be9a169aa134d1
Kimberlyka24 <kimberlyka24@gmail.com>
2021-02-01 16:45:39 +0800

    added my greetings

--- src/acama/Main.java
+++ src/acama/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Kate.");
+        System.out.println("Hello Kate, my name is Kim!");
     }
 }
