cff5c22ca008e70db71d8459812614f7669769a9
Richelle Anne Natividad <richellenatividad@gmail.com>
2021-03-27 00:51:13 -0700

    added a line

--- src/acama/Main.java
+++ src/acama/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Kate.");
         System.out.println("Hello Kate, my name is Kim!");
+        System.out.println("Hello Kate, my name is Richelle!");
     }
 }
